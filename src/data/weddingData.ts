978b011538f544cd4050234d80a1e3c47e6d2c47
user <user@Mac-mini-3.lan>
2026-09-18 10:46:04 +0600

    Update groom's mother name to Aklima Begum

--- src/data/weddingData.ts
+++ src/data/weddingData.ts
@@ -40,7 +40,7 @@ export const weddingData: WeddingDetails = {
   brideName: "Maisha Imrose Tanisha",
   brideFamily: "Daughter of Yasin Chowdhury & Rafeya Sultana",
   groomName: "Yeasir Rahman Toukir",
-  groomFamily: "elder son of Ismail Jabiullah & Akima Akter",
+  groomFamily: "elder son of Ismail Jabiullah & Aklima Begum",
   weddingDate: "September 27, 2026",
   weddingTime: "7:30 PM",
   venue: "Officers Club Chattogram",
